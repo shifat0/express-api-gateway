804919c557c8052075b97f2c735146e519ca3fa9
shifat0 <sifuisu3@gmail.com>
2024-11-13 00:47:28 +0600

    proxyHandler is modified and added pathfilter, config.json is also modified

--- src/middlewares/proxyHandler.ts
+++ src/middlewares/proxyHandler.ts
@@ -1,15 +1,17 @@
 import { Application } from 'express';
 import config from '@/configs/config.json';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import envConfig from '@/configs/envConfig';
 
 export default function proxyHandler(app: Application) {
     Object.entries(config.services).forEach(([name, service]) => {
         const proxy = createProxyMiddleware({
-            target: `http://localhost:${service.port}/${service.endpoint}`,
+            target: `http://localhost:${service.port}`,
             changeOrigin: true,
+            pathFilter: `${envConfig.API_VERSION}${service.endpoint}`,
             logger: console
         });
 
-        app.use(service.endpoint, proxy);
+        app.use(proxy);
     });
 }
